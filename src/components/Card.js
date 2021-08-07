5aa20eac6c9fd723e07d364382482c1644df3e3c
Derek <derek.huynen@gmail.com>
2021-08-07 15:35:29 -0700

    Update phone view

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../css/Card.css'
 
-function HouseCard({image1, title}) {
+function HouseCard({image1, title, icon}) {
 
     return (
         <div className='card' >
             <img src={image1} alt=""/>
             <div className="card__info">
-                <h2>{title}</h2>
+                <div className={'card__title'}>
+                    <h2 style={{float: 'left'}}>{title}</h2>
+                    <h2 style={{float: 'right'}}>{icon}</h2>
+                </div>
             </div>
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,22 +4,22 @@ import Banner from '../components/Banner'
 import HouseCard from '../components/HouseCard'
 import Card from '../components/Card'
 import PapaBear720_1 from '../CabinPhotos/PapaBear720/PapaBear720-1.JPG'
-import PapaBear720_2 from '../CabinPhotos/PapaBear720/PapaBear720-2.JPG'
 
 import Scandia720_1 from '../CabinPhotos/Scandia720/Scandia720-1.JPG'
-import Scandia720_2 from '../CabinPhotos/Scandia720/Scandia720-2.JPG'
 
 import TreeHutte720_1 from '../CabinPhotos/TreeHutte720/TreeHutte720-24.JPG'
-import TreeHutte720_2 from '../CabinPhotos/TreeHutte720/TreeHutte720-2.JPG'
 
 import WhisperingPines720_1 from '../CabinPhotos/WhsiperingPines720/WhisperingPines720-1.JPG'
-import WhisperingPines720_2 from '../CabinPhotos/WhsiperingPines720/WhisperingPines720-2.JPG'
 
 import GoldRush720_1 from '../CabinPhotos/GoldRush720/GoldRush720-1.JPG'
-import GoldRush720_2 from '../CabinPhotos/GoldRush720/GoldRush720-2.JPG'
 
 import Hiking720 from '../CabinPhotos/City720/City720-1.JPG'
 import Village720 from '../CabinPhotos/City720/City720-2.JPG'
+
+import { Icon } from '@iconify/react';
+import calendarEvent from '@iconify/icons-bi/calendar-event';
+import baselineHiking from '@iconify/icons-ic/baseline-hiking';
+import iRestaurant from '@iconify/icons-medical-icon/i-restaurant';
 
 
 function Home() {
@@ -30,69 +30,69 @@ function Home() {
             <div className='home__section'>
                 <Card
                     image1={GoldRush720_1}
-                    title="All Cabins"
+                    title="Current Events"
+                    icon={<Icon icon={calendarEvent} width="20" height="20" inline={true}/>}
                 />
                 <Card
                     image1= {Hiking720}
                     title="Things To Do"
+                    icon={<Icon icon={baselineHiking} width="20" height="20" inline={true}/>}
                 />
                 <Card
                     image1={Village720}
                     title="Restaurants"
+                    icon={<Icon icon={iRestaurant} width="20" height="20" inline={true}/>}
                 />
             </div>
             <div className='home__section'>
                 <HouseCard
                     image1={WhisperingPines720_1}
-                    image2={WhisperingPines720_2}
                     title="Whispering Pines"
                     guests= "4"
                     bedrooms= "2"
                     bathrooms= "2"
                     license="2021-1234"
                     link="https://airbnb.com/h/whispheringpines"
-
+                    rating="4.92 (73 reviews)"
                 />
                 <HouseCard
                     image1={Scandia720_1}
-                    image2={Scandia720_2}
                     title="Scandia Chalet"
                     guests= "10"
                     bedrooms= "3"
                     bathrooms= "2.5"
                     license="2021-1234"
                     link="https://airbnb.com/h/chaletscandia"
+                    rating="4.75 (4 reviews)"
                 />
                 <HouseCard
                     image1={PapaBear720_1}
-                    image2={PapaBear720_2}
                     title="Papa Bear Cabin"
                     guests= "5"
                     bedrooms= "2"
                     bathrooms= "2"
-                    license="2021-1234"
+                    license="2021-0078"
                     link="https://airbnb.com/h/papabearcabin"
+                    rating="5.00 (2 reviews)"
                 />
                 <HouseCard
                     image1={TreeHutte720_1}
-                    image2={TreeHutte720_2}
                     title="Tree Hütte"
                     guests= "5"
                     bedrooms= "3"
                     bathrooms= "1"
                     license="2021-1234"
                     link="https://airbnb.com/h/treehutte"
+                    rating="4.88 (8 reviews)"
                 />
                 <HouseCard
                     image1={GoldRush720_1}
-                    image2={GoldRush720_2}
                     title="Gold Rush Cabin"
                     guests= "14"
                     bedrooms= "3"
                     bathrooms= "2.5"
                     license="2021-1234"
-                    link="https://airbnb.com/h/treehutte"
-                />
+                    link="https://airbnb.com/h/treehutte"/>
             </div>
         </div>
     )
