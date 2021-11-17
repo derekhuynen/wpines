a2535db1553a4145718e7578abd07704825f6b0b
derekhuynen <Derek.huynen@gmail.com>
2021-11-17 08:06:52 -0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ import iRestaurant from '@iconify/icons-medical-icon/i-restaurant';
 import houseDoor from '@iconify/icons-bi/house-door';
 
 import {cabins} from '../JSON/cabins'
-import OctoberFest from "../components/Banners/Octoberfest";
+
 
 
 function Home() {
