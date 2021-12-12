ae3b63bfaf18a2bfc6f4b4e2828861b0bf9ab99d
derekhuynen <Derek.huynen@gmail.com>
2021-12-11 16:39:47 -0800

    Update Test.js

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -11,54 +11,54 @@ export default function Test() {
 
 
         <div className='outerContainer'>
-            <div className='contactForm'>
-                <form id='contact-form' noValidate className={'form'}>
-                    {/* Row 1 of form */}
-                    <div className='rowformRow'>
-                        <div className='col-6'>
-                            <input
-                                type='text'
-                                name='name'
-                                className='formInputOne'
-                                placeholder='Name'
-                            />
-                        </div>
-                        <div className='col-6'>
-                            <input
-                                type='email'
-                                name='email'
-                                className='formInputOne'
-                                placeholder='Email address'
-                            />
-                        </div>
-                    </div>
-                    {/* Row 2 of form */}
-                    <div className='rowformRow'>
-                        <div className='col'>
-                            <input
-                                type='text'
-                                name='subject'
-                                className='formInputSubject'
-                                placeholder='Subject'
-                            />
-                        </div>
-                    </div>
-                    {/* Row 3 of form */}
-                    <div className='rowformRow'>
-                        <div className='colMessage'>
-                    <textarea
-                        rows={5}
-                        name='message'
-                        className='form-controlMessage'
-                        placeholder='Message'
-                    />
-                        </div>
-                    </div>
-                    <div className={"buttonDiv"}>
-                        <Button variant='outlined' type='submit'>Submit</Button>
-                    </div>
-                </form>
-            </div>
+            {/*<div className='contactForm'>*/}
+            {/*    <form id='contact-form' noValidate className={'form'}>*/}
+            {/*        /!* Row 1 of form *!/*/}
+            {/*        <div className='rowformRow'>*/}
+            {/*            <div className='col-6'>*/}
+            {/*                <input*/}
+            {/*                    type='text'*/}
+            {/*                    name='name'*/}
+            {/*                    className='formInputOne'*/}
+            {/*                    placeholder='Name'*/}
+            {/*                />*/}
+            {/*            </div>*/}
+            {/*            <div className='col-6'>*/}
+            {/*                <input*/}
+            {/*                    type='email'*/}
+            {/*                    name='email'*/}
+            {/*                    className='formInputOne'*/}
+            {/*                    placeholder='Email address'*/}
+            {/*                />*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*        /!* Row 2 of form *!/*/}
+            {/*        <div className='rowformRow'>*/}
+            {/*            <div className='col'>*/}
+            {/*                <input*/}
+            {/*                    type='text'*/}
+            {/*                    name='subject'*/}
+            {/*                    className='formInputSubject'*/}
+            {/*                    placeholder='Subject'*/}
+            {/*                />*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*        /!* Row 3 of form *!/*/}
+            {/*        <div className='rowformRow'>*/}
+            {/*            <div className='colMessage'>*/}
+            {/*        <textarea*/}
+            {/*            rows={5}*/}
+            {/*            name='message'*/}
+            {/*            className='form-controlMessage'*/}
+            {/*            placeholder='Message'*/}
+            {/*        />*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*        <div className={"buttonDiv"}>*/}
+            {/*            <Button variant='outlined' type='submit'>Submit</Button>*/}
+            {/*        </div>*/}
+            {/*    </form>*/}
+            {/*</div>*/}
 
             <div className={"bottomSection"}>
             <div className={"bottomCenter"}>
